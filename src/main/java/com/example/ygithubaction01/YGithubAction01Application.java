5ac8ffb585e1405000db5ad96e57e5ff8b8641bd
ERLMKea <erlm@kea.dk>
2023-01-02 18:07:26 +0100

    Initial commit

---
+++ src/main/java/com/example/ygithubaction01/YGithubAction01Application.java
@@ -0,0 +1,13 @@
+package com.example.ygithubaction01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YGithubAction01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YGithubAction01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ygithubaction01/YGithubAction01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ygithubaction01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YGithubAction01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
